8c035817e73e3470a1cd2f1dc1ef75dc3f52c376
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-12-24 13:12:46 +0200

    Added success message display when user add a to do to the list

--- js/script.js
+++ js/script.js
@@ -2,12 +2,31 @@ const input = document.querySelector(".input");
 const inputForm = document.querySelector("form");
 const submitBtn = document.querySelector(".submit-btn");
 const todosContainer = document.querySelector(".todos-container");
-let todoListItemsArray = [];
+const alert = document.querySelector(".alert");
+// app state
+const state = {
+  todoListItemsArray: [{ todoItem: "ham", id: 5 }],
+};
+let todoListItemsArray = [{ todoItem: "ham", id: 5 }];
+let isAlertVisible = false;
+let isEditing = false;
 /**
  * Clears the to do input field after the user adds the to do to the list of to dos
  */
 const clearTodoInputField = () => {
   input.value = "";
+};
+const displayAlert = () => {
+  isAlertVisible = true;
+  if (isAlertVisible) {
+    alert.classList.add("alert-success", "show-alert");
+    alert.textContent = `Successfully added to do to list!`;
+    setTimeout(() => {
+      isAlertVisible = false;
+      alert.classList.remove("alert-success", "show-alert");
+    }, 3000);
+    //cleartimeout
+  }
 };
 /**
  * Adds input to do value in the list of to dos to be displayed.
@@ -19,24 +38,44 @@ const addTodoItemToList = () => {
       ...todoListItemsArray,
       { todoItem, id: crypto.randomUUID() },
     ];
+    displayAlert();
   }
   clearTodoInputField();
-  console.log(todoListItemsArray);
 };
+/**
+ * Displays to do when they are added to the to dos list.
+ */
 const displayTodoListItems = () => {
   const fragment = new DocumentFragment();
   todoListItemsArray.forEach((todo) => {
     const todoItem = document.createElement("li");
     todoItem.classList.add("todoListItem");
     todoItem.innerHTML = `
-   <div>
+   
    <p>${todo.todoItem}</p>
-   </div>`;
+   <div class="todoItemIcons">
+   <button data-action="edit">Edit</button>
+   <button data-action="delete">Delete</button>
+   </div>
+   `;
+    todoItem.addEventListener("click", (e) => {
+      
+      const actionType = e.target.dataset.action;
+      if(actionType ==="edit"){
+        console.log("EDITTING")
+      }
+      if(actionType ==="delete"){
+        console.log("DELETING")
+      }
+    });
+    //    <i class="fa-solid fa-trash"></i>
+    //   <i class="fa-solid fa-pen-to-square"></i>
     fragment.appendChild(todoItem);
   });
   todosContainer.innerHTML = "";
   todosContainer.appendChild(fragment);
 };
+displayTodoListItems();
 inputForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodoItemToList();
